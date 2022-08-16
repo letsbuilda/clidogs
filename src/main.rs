54c63abc468fb09f9b371ab82e58ae0d104d8aac
SavagePastaMan <69145546+SavagePastaMan@users.noreply.github.com>
2022-08-16 10:52:07 -0400

    Added options for rainbow and random colors
    Default coloring is no colors.
    
    Need to DRY code, probably use a function that prints lines from an
    iterator. If more options are added that modify the text somehow, they would
    operate line by line on the iterator.

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,44 @@
-use clap::{crate_authors, crate_version, Command};
+use clap::{builder::Arg, crate_authors, crate_version, Command};
 use owo_colors::{DynColors, OwoColorize};
 use rand::seq::SliceRandom;
 
 static DOG1: &str = include_str!("./art/dog1.txt");
 
 fn main() {
+    let colors: [DynColors; 7] = [
+        "#B80A41", // Red
+        "#E23838", // Orange
+        "#DAAC06", // Yellow
+        "#6EB122", // Green
+        "#00938A", // Blue
+        "#4E4BA8", // Indigo
+        "#BA55D3", // Purple
+    ]
+    .map(|color| color.parse().unwrap());
+
     let command = Command::new("clidogs")
         .about("∪･ω･U")
         .version(crate_version!())
-        .author(crate_authors!("\n"));
+        .author(crate_authors!("\n"))
+        .arg(Arg::new("rainbow").long("rainbow"))
+        .arg(Arg::new("random").long("random").conflicts_with("rainbow"));
+
+    let lines: Vec<_> = DOG1.lines().collect();
 
     let matches = command.get_matches();
 
-    if !matches.args_present() {
-        let colors: [DynColors; 6] = [
-            "#B80A41", "#4E4BA8", "#6EB122", "#DAAC06", "#00938A", "#E23838",
-        ]
-        .map(|color| color.parse().unwrap());
-
+    if matches.contains_id("rainbow") {
+        lines
+            .iter()
+            .zip(colors.iter().cycle())
+            .map(|(line, color)| line.color(*color))
+            .for_each(|line| println!("{}", line));
+    } else if matches.contains_id("random") {
         let mut rng = rand::thread_rng();
-
-        for line in DOG1.lines() {
-            let color = colors.choose(&mut rng).unwrap();
-            println!("{}", line.color(*color));
+        for line in lines {
+            println!("{}", line.color(*colors.choose(&mut rng).unwrap()));
         }
+    } else {
+        lines.iter().for_each(|line| println!("{}", line));
     }
 }
